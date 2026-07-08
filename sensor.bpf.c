2694dc68dea160192052cdf6a707a49a7d6af49b
apexajay-rc <ajaykrishnanh.dev@gmail.com>
2026-07-08 23:36:29 +0530

    Update sensor.bpf.c

--- sensor.bpf.c
+++ sensor.bpf.c
@@ -2,31 +2,46 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 
-// 2. The Hook (Where we listen)
-// SEC (Section) tells the compiler to put this function in a specific part of the binary
-// so the kernel knows exactly where to attach it. We are listening to 'execve' (when a program runs).
+// 2. The Data Structure (Our Payload)
+// This dictates the exact byte layout of the data we are sending to Python.
+struct event_t {
+    u32 pid;            // 32-bit Process ID
+    char comm[16];      // 16-character array for the Command Name
+};
+
+// 3. The Ring Buffer Map (The Bridge)
+// We tell the kernel to carve out a 256 KB chunk of memory for our queue.
+struct {
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 256 * 1024);
+} events SEC(".maps");
+
+
 SEC("tracepoint/syscalls/sys_enter_execve")
 int bpf_sentry_hello(void *ctx) {
-    
-    // 3. The BPF Helpers
-    // We cannot use standard OS commands, so we ask the kernel directly who is running.
-    // It returns a 64-bit integer.
     u64 id = bpf_get_current_pid_tgid();
-    
-    // 4. Bitwise Operations
-    // The kernel packs two pieces of data into that 64-bit number to save space.
-    // Thread Group ID (TGID) is the top 32 bits, and the Thread ID (PID) is the bottom 32 bits.
-    // We shift the bits 32 spaces to the right (>>) to isolate the PID.
     u32 pid = id >> 32;
 
-    // 5. The Output
-    // We cannot use printf(). We use the kernel's internal logger.
-    bpf_printk("SENTRY BASE CAMP COMPLETE. Target PID: %d executing.\\n", pid);
+    // 4. Pointer & Memory Reservation
+    // Ask the kernel to reserve enough space in the Ring Buffer for one 'event_t' struct.
+    // We get a pointer (*e) pointing to that reserved memory address.
+    struct event_t *e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
+    
+    // If the buffer is completely full, 'e' will be NULL. We just drop the event and exit.
+    if (!e) {
+        return 0; 
+    }
 
-    // The Verifier demands that we always return 0 to prove the function finishes.
+    // 5. Fill the Struct
+    e->pid = pid;
+    // BPF Helper to grab the name of the program (e.g., "bash", "python", "htop")
+    bpf_get_current_comm(&e->comm, sizeof(e->comm)); 
+
+    // 6. Submit to Python
+    // Send the filled struct across the bridge to User Space.
+    bpf_ringbuf_submit(e, 0);
+
     return 0;
 }
 
-// 6. The Bouncer's Requirement
-// The Linux kernel strictly refuses to load eBPF code unless it has a GPL-compatible license.
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
